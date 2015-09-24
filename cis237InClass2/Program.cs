2ef8d303d1b5c464122dc00268bbe23aab89f408
Yihan Wang <ywang4241@mail.kvcc.edu>
2015-09-24 14:12:12 -0400

    Add factorial class

--- cis237InClass2/Program.cs
+++ cis237InClass2/Program.cs
@@ -10,6 +10,11 @@ namespace cis237InClass2
     {
         static void Main(string[] args)
         {
+
+            Factorial factorial = new Factorial();
+
+            Console.WriteLine("The answer to 5! is: ");
+            Console.WriteLine(factorial.Calculate(5));
         }
     }
 }
